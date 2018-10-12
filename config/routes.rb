26d12dfb4a3ffde8cb0f2dd6d3de927d2aa0ca8b
Cristian Yanza <cristian.yanza@wolox.co>
2018-10-12 15:49:11 -0500

    Fix migration json

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # token auth routes available at /api/v1/auth
-  namespace :api do
-    scope :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
-    end
+  api_version(module: 'api/v1', path: { value: 'api/v1' }) do
+    mount_devise_token_auth_for 'User', at: 'auth'
+    #resources :books, only: [:index, :show]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+ end
