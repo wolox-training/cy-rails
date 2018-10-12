0436f619b7b1daa119ad6c6798fd4ec8b4561a59
Cristian Yanza <cristian.yanza@wolox.co>
2018-10-12 11:04:49 -0500

    Fix version 2 validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   devise  :database_authenticatable, :registerable, :recoverable, :rememberable,
           :trackable, :validatable
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, :last_name, presence: true
 end
