35b58a549fb10414ed00e4ba167e1004b735df35
Cristian Yanza <cristian.yanza@wolox.co>
2018-10-16 08:45:38 -0500

    Fix for comma

--- db/migrate/20181016131809_add_provider_uid_to_users.rb
+++ db/migrate/20181016131809_add_provider_uid_to_users.rb
@@ -2,6 +2,6 @@ class AddProviderUidToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :provider, :string, :default => "email", :null => false
     add_column :users, :uid, :string, :default => "", :null => false
-    add_index :users, [:uid, :provider] unique: true
+    add_index :users, [:uid, :provider], unique: true
   end
 end
