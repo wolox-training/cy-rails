0481a950485676ea492cc8cc47efc4bfb618644f
Cristian Yanza <cristian.yanza@wolox.co>
2018-10-16 08:42:04 -0500

    Fix default

--- db/migrate/20181016125121_add_confirmable_to_users.rb
+++ db/migrate/20181016125121_add_confirmable_to_users.rb
@@ -1,6 +1,6 @@
 class AddConfirmableToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :sign_in_count, :integer, default => 0, :null => false
+    add_column :users, :sign_in_count, :integer, :default => 0, :null => false
     add_column :users, :current_sign_in_at, :datetime
     add_column :users, :last_sign_in_at, :string
     add_column :users, :last_sign_in_ip, :string
