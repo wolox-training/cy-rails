7c68f7ad38b47ab5f760590e505b213b40fe0ac0
Cristian Yanza <cristian.yanza@wolox.co>
2018-10-23 15:01:26 -0500

    Improvement auth token

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class BooksController < ApplicationController
+      before_action :authenticate_api_v1_user!
       def show
         @book = Book.find(params[:id])
         render_paginated json: @book
