1be9687a4a123e4c4fff0c1a6903de3126b58625
Cristian Yanza <cristian.yanza@wolox.co>
2018-10-26 14:06:54 -0500

    Fix devise include

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules.
-  include DeviseTokenAuth::Concerns::User
   devise  :database_authenticatable, :registerable, :recoverable, :rememberable,
           :trackable, :validatable
   validates :first_name, :last_name, presence: true
   has_many :rents, dependent: :nullify
+  include DeviseTokenAuth::Concerns::User
 end
