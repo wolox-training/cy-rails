4868371244bdc970601d608d00d6f25febfeaf6d
Cristian Yanza <cristian.yanza@wolox.co>
2018-11-07 14:56:38 -0500

    fix internaEmail user and book id

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,8 +1,8 @@
 class RentMailer < ApplicationMailer
   def new_rent(rent)
     @rent = rent
-    @book = Book.find(@rent.book_id)
-    @user = User.find(@rent.user_id)
+    @book = Book.find(@rent.book)
+    @user = User.find(@rent.user)
     mail(to: @user.email, subject: t('send_mail.mail_new_rent'))
   end
 end
