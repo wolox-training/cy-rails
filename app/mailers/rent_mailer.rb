a0a31740770d6762af7ce0530a52562437fde9a2
Cristian Yanza <cristian.yanza@wolox.co>
2018-11-08 13:52:07 -0500

    Fix activeAdmin, validation for send email

--- app/mailers/rent_mailer.rb
+++ app/mailers/rent_mailer.rb
@@ -1,8 +1,9 @@
 class RentMailer < ApplicationMailer
   def new_rent(rent)
     @rent = rent
-    @book = Book.find(@rent.book_id)
-    @user = User.find(@rent.user_id)
-    mail(to: @user.email, subject: t('send_mail.mail_new_rent'))
+    @book = @rent.book
+    @user = @rent.user
+    subject = t('send_mail.mail_new_rent')
+    mail(to: @user.email, subject: subject) if @book.present? && @rent.present?
   end
 end
